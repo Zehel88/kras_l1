74a11b7f53151daac6e6332d7e7233103b869ce7
Zehel88 <Vladmiloserdov@yandex.ru>
2015-12-09 00:56:16 +0400

    Tolik release

--- kras_lab1_main2.m
+++ kras_lab1_main2.m
@@ -45,15 +45,12 @@ end
 
 
 % --- Executes just before kras_lab1_main2 is made visible.
-function [X Y Z]=kras_lab1_main2_OpeningFcn(hObject, eventdata, handles, varargin)
+function kras_lab1_main2_OpeningFcn(hObject, eventdata, handles, varargin)
 
 handles.output = hObject;
 % Update handles structure
 guidata(hObject, handles);
-
 clc
-
-
 % ====================================================================
 syms p1 p2
 % Заданная функция
@@ -73,8 +70,8 @@ xp2=double(Sres.p2);
 in_f=inline(f);
 f_z=in_f(xp1(2),xp2(2));
 % Cтроим линию уровня соответствующую найденному значению
-x=-1:0.05:1;
-y=-1:0.05:1;
+x=-0.5:0.001:0.5;
+y=-0.5:0.001:0.5;
 [X,Y]=meshgrid(x,y);
 Z=7.*X.^2+4.*X.*Y+Y.^2-Y.^3;
 % Z=X.*Y.*-5.0+X.^2.*6.0+X.^3.*3.0+Y.^2.*2.0;
@@ -85,68 +82,141 @@ contour(X,Y,Z,[f_z f_z]);grid on;hold on;
 % Вписываем окружность, как многообразие с простым атласом
 % CR=0.2;
 CR=0.27;
-plotCircle(0,0,CR);xlabel(['Рис.1 ','Многообразие и окружность M_0=S^1(0,',num2str(CR),')']);
+plotCircle(0,0,CR);xlabel(['Многообразие и окружность M_0=S^1(0,',num2str(CR),')']);
 
 
 %% Находим коэффициент пропорциональности
-syms a1 x1 x2 y1 a2 z1
+syms a x1 x2 y b z
 % Ф-я в локальных координатах карты 1
-fa1=subs(f,[p1 p2],[a1.*y1 a1.*sqrt(CR^2-y1.^2)]);
+fa=(collect(subs(f,[p1 p2],[y*a a*sqrt(CR^2-y^2)]),a))-f_z;
+
+
+[c,p]=coeffs(fa,a);
+np=find(sym2poly(sum(p))==1);
+
 
 % Нахождение коэф. пропорц. для 1 карты
-j=1;
-for i=-CR:(2*CR/50):CR
-    fi1=subs(fa1-CR,y1,i);
-    r=roots(sym2poly(fi1));
-    t1=find(r>0);
-    a1(j)=min(r(t1));
-    k1(j)=i;
-    j=j+1; 
-end
-% Ф-я в локальных координатах карты 2
-fa2=subs(f,[p1 p2],[a2.*sqrt(CR^2-z1.^2) a2.*z1]);
+n=50;
 
-% Нахождение коэф. пропорц. для 2 карты
+
 j=1;
-for i=-CR:(2*CR/50):CR
-    fi2=subs(fa2-CR,z1,i);
-    r=roots(sym2poly(fi2));
-    t1=find(r>0);
-    a2(j)=min(r(t1));
-    k2(j)=i;
-    j=j+1;   
+for i=-CR:(2*CR/n):CR
+%   Замена символьных переменных 
+    w(np)=double(subs(c,y,i));
+%   Нахождение корней многочлена
+    r=(roots(w));
+%   Нахождение корней >0
+    r=r(find(r>0));
+%   Нахождение корней <1
+%     r=r(find(r<1));
+
+%   Нахождение действительных корней  
+a(j)=min(r);
+%     try
+%     a(j)=r(find(imag(r)==0));
+%     catch
+%     j=j-1;
+%     end
+%   буферные переменные
+    k1(j)=i;
+    j=j+1;
 end
 % Построение гарфика коэф. проп 1 карты
 axes(handles.axes6)
-plot(k1,a1);grid on;xlabel({'Коэф. пропорциональности ','для карты (U_{1},\phi_{1})'});
+plot(k1,a);grid on;xlabel({'Коэф. пропорциональности ','для карты (U_{1},\phi_{1})'});
+
+%% Ф-я в локальных координатах карты 2
+fb=subs(f,[p1 p2],[b.*sqrt(CR^2-z.^2) b.*z])-f_z;
+
+[c,p]=coeffs(fb,b);
+np=find(sym2poly(sum(p))==1);
+
+% Нахождение коэф. пропорц. для 2 карты
+j=1;
+for i=CR:-(2*CR/n):-CR
+%  Замена символьных переменных 
+    w(np)=double(subs(c,z,i));
+
+%   Нахождение корней многочлена
+    r=(roots(w));
+%   Нахождение корней >0
+    r=r(find(r>0));
+%   Нахождение корней <1
+%     r=r(find(r<1));
+%   Нахождение действительных корней  
+b(j)=min(r);
+%     try
+%     b(j)=r(find(imag(r)==0));
+%     catch
+%     j=j-1;
+%     end
+%   буферные переменные
+    k2(j)=i;
+    j=j+1;
+end
 % Построение гарфика коэф. проп 2 карты
 axes(handles.axes7)
-plot(k2,a2);grid on;xlabel({'Коэф. пропорциональности ','для карты (U_{2},\phi_{2})'});
+plot(k2,b);grid on;xlabel({'Коэф. пропорциональности ','для карты (U_{2},\phi_{2})'});
 
-% Построение функции в координатном пространстве {G1(y),y}
-axes(handles.axes2)
-fy=-CR:(2*CR/50):CR;
+%% Построение функции в координатном пространстве {G1(y),y}
+axes(handles.axes8)
 % fy=sqrt(CR^2-y.^2);
-for i=1:numel(fy)
+
+for i=1:numel(k1)
 % G1(i)=a1(i)^4*fy(i)^3*sqrt(CR^2-fy(i)^2)+a1(i)^3*(CR^2-fy(i)^2)*fy(i)-a1(i)^5*(CR^2-fy(i)^2)^(5/2);
-G1(i)=7.*(a1(i).*fy(i)).^2+4.*(a1(i).*fy(i)).*(a1(i)*sqrt(CR^2-fy(i).^2))+(a1(i)*sqrt(CR^2-fy(i).^2)).^2-(a1(i)*sqrt(CR^2-fy(i).^2)).^3;
+p1=a(i)*k1(i);
+p2=a(i)*sqrt(CR^2-k1(i)^2);
+G1(i)=7*p1^2+4*p1*p2+p2^2-p2^3;
 end
 
-plot(fy,G1);grid on;
-xlabel({'График функции F(p)',' в координатах карты (U_{1},\phi_{1})'})
+plot(k1,G1);grid on;
+xlabel({'График функции F(p)',' в координатах карты (U_{1},\phi_{1})'});
+hold on
 
-% Построение функции в координатном пространстве {G2(z),z}
-axes(handles.axes3)
-fz=-CR:(2*CR/50):CR;
+%% Построение функции в координатном пространстве {G2(z),z}
+
 % fz=sqrt(CR^2-z.^2);
-for i=1:numel(fz)
+for i=1:numel(k2)
 % G2(i)=a2(i)^4*fz(i)*(CR^2-fz(i)^2)^(3/2)+a2(i)^3*fz(i)^2*sqrt(CR^2-fz(i)^2)-a2(i)^5*fz(i)^5;
-G2(i)=7.*(a2(i)*(sqrt(CR^2-fz(i)^2))).^2+4.*(a2(i)*(sqrt(CR^2-fz(i)^2))).*(a2(i)*fz(i)^2)+(a2(i)*fz(i)^2).^2-(a2(i)*fz(i)^2).^3;
+p1=b(i)*(sqrt(CR^2-k2(i)^2));
+p2=b(i)*k2(i);
+G2(i)=7*p1^2+4*p1*p2+p2^2-p2^3;
 end
 
-plot(fz,G2);grid on;
+plot(k2,G2);grid on;
 xlabel({'График функции F(p)' ,'в координатах карты  (U_{2},\phi_{2})'})
+%% 
 
+axes(handles.axes2)
+y=CR.*cos(pi:-pi/n:0);
+numel(a)
+numel(y)
+p_1=a.*y;
+p_2=a.*(sqrt(CR^2-y.^2));
+G_1=7.*p_1.^2+4.*p_1.*p_2+p_2.^2-p_2.^3;
+
+plot(y,G_1);
+grid on
+
+axes(handles.axes3)
+z=CR.*sin(pi/2:-pi/n:-pi/2);
+
+p__1=b.*(sqrt(CR^2-z.^2));
+p__2=b.*z;
+figure(1)
+plot(p__1)
+figure(2)
+plot(p__2)
+G_2=7.*p__1.^2+4.*p__1.*p__2+p__2.^2-p__2.^3;
+
+plot(z,G_2);
+grid on
+
+DaTa.a=a;
+DaTa.b=b;
+save('DaTa.mat','DaTa');
+
+%% 
 % Функция для задания 3
 % x_range=-1:0.05:1;
 % y_range=-1:0.05:1;
@@ -158,11 +228,11 @@ xlabel({'График функции F(p)' ,'в координатах карты  (U_{2},\phi_{2})'})
 % mesh(p31,p32,f3,'UIContextMenu',f2pCM2);
 % xlabel('График функции F(p)                     ')
 
-DaTa.X=X;
-DaTa.Y=Y;
-DaTa.Z=Z;
+% DaTa.X=X;
+% DaTa.Y=Y;
+% DaTa.Z=Z;
 
-save('DaTa.mat','DaTa');
+% save('DaTa.mat','DaTa');
 
 
 % UIWAIT makes kras_lab1_main2 wait for user response (see UIRESUME)
